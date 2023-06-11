95188927286ac6acc82dc04a13740362da123008
Vishnu.G <119050104+Vishnupriyang@users.noreply.github.com>
2023-06-11 06:04:39 +0530

    channelplay.py

--- VishnuX/utils/channelplay.py
+++ VishnuX/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonX import app
-from AnonX.utils.database import get_cmode
+from VishnuX import app
+from VishnuX.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
